5a94238cb39b1e2e70ad55055e9092dc86746ca5
David Taylor <david.a.taylor253@gmail.com>
2026-01-23 17:03:20 -0800

    Add initial data objects for NotecardScreen.

--- app/src/main/java/com/andaagi/learnandroiddev/viewmodel/NotecardViewModel.kt
+++ app/src/main/java/com/andaagi/learnandroiddev/viewmodel/NotecardViewModel.kt
@@ -1,7 +1,11 @@
 package com.andaagi.learnandroiddev.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.andaagi.learnandroiddev.state.NotecardScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -9,7 +13,12 @@ class NotecardViewModel @Inject constructor(
 
 ): ViewModel() {
 
+    private val _state = MutableStateFlow(NotecardScreenState())
+    val state: StateFlow<NotecardScreenState> = _state
+
     //TODO grab all sets of notecards from the database
+
+    //TODO grab all notecards in a set from the database
 
     //TODO state of the set selector -> should it be showing or not?
 
